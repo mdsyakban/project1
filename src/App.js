8e2730968ed6b409e82a83d8a616f8fb4910c8c6
Ahmad Syakban <ahmadsyakban240@gmail.com>
2022-08-23 21:35:10 +0700

    Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Routes, Route } from 'react-router-dom';
+import Film from './componen/Film';
+import Gallery from './componen/Gallery';
+import Home from './componen/Home';
+import Music from './componen/Mucis';
+import Profile from './componen/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ul>
+      <li> <Link to="/">Home</Link> </li>
+      <li> <Link to="/profile">Profile</Link> </li>
+      <li> <Link to="/gallery">Gallery</Link> </li>
+      <li> <Link to="/music">Music</Link> </li>
+      <li> <Link to="/film">Film</Link> </li>
+    </ul>
+
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/gallery' element={<Gallery />} />
+      <Route path='/music' element={<Music />} />
+      <Route path='/film' element={<Film />} />
+    </Routes>
+    </>
   );
 }
 
